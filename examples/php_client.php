f5ac2a0bb95b64efd26129bafc18deb1b849a176
Jason Martin <jason.martin83@protonmail.com>
2018-05-16 12:11:19 -0400

    Formatting code
    Signed-off-by: Jason Martin <jason.martin83@protonmail.com>

--- examples/php_client.php
+++ examples/php_client.php
@@ -4,21 +4,18 @@ $port = getenv("GONAD_PORT");
 echo "host=$host, port=$port\n";
 $s = microtime(true);
 $socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
-
-	if ( false === $socket ) {
-		die("failed to open socket");
-	}
-	$r = socket_connect($socket,$host,$port);
-	if ( false === $r ) {
-		die("failed to connect");
-	}
+if ( false === $socket ) {
+	die("failed to open socket");
+}
+$r = socket_connect($socket,$host,$port);
+if ( false === $r ) {
+	die("failed to connect");
+}
 for ( $i = 0; $i <= 1000; $i++ ) {
-	
-	
 	$msg = "loop=$i\n";
 	socket_write($socket,$msg,strlen($msg));
 }
-	socket_close($socket);
+socket_close($socket);
 $e = microtime(true);
 echo $e - $s . "\n";
 
